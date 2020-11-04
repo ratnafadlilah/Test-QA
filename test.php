abe3566db412285d8deadd2899d29228bcff6fcf
Ratna Fadlilah <ratnafadlilah0@gmail.com>
2020-11-04 12:52:38 +0700

    test qa

---
+++ test.php
@@ -0,0 +1,66 @@
+<DOCTYPE html>
+<html>
+<body>
+
+<?php
+class Company {
+    var $name;
+    function setCompanyName($name){
+        $this->name = $name;
+    }
+    function getCompanyName(){
+        return $this->name;
+    }
+}
+
+class Department {
+    var $name;
+    function setDepartmentName($name){
+        $this->name = $name;
+    }
+    function getDepartmentName(){
+        return $this->name;
+    }
+}
+
+class Employee {
+    var $name;
+    var $title;
+    var $salary;
+    function setEmployeeName($name){
+        $this->name = $name;
+    }
+    function getEmployeeTitle(){
+        return $this->title;
+    }
+    function getEmployeeProfile(){
+        return $this->name;
+    }
+    function getEmployeeMonthlySalary(){
+        return $this->salary;
+    }
+}
+
+$name = new Company();
+$setCompanyName = $name->setCompanyName("DOT");
+$getCompanyName = $name->getCompanyName();
+echo $getCompanyName;
+
+$name = new Department();
+$setDepartmentName = $name->setDepartmentName("Development");
+$getDepartmentName = $name->getDepartmentName();
+echo $getDepartmentName;
+
+$name = new Employee();
+$setEmployeeName = $name->setEmployeeName("DOT");
+$getEmployeeTitle = $title->getEmployeeTitle();
+$getEmployeeProfile = $name->getEmployeeProfile();
+$getEmployeeMonthlySalary = $salary->getEmployeeMonthlySalary();
+echo $getEmployeeName;
+echo $getEmployeeTitle;
+echo $getEmployeeProfile;
+echo $getEmployeeMonthlySalary;
+
+?>
+</body>
+</html>
